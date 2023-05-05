d03c444532f3ad50c71b274e2cc3ea2ee48c6884
Ross <oblivion1214@hotmail.com>
2023-05-04 20:54:05 -0400

    Added env file

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const app = express();
 
-const port = 8000;
+import * as dotenv from 'dotenv';
+import express from 'express';
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
-  })
+});
